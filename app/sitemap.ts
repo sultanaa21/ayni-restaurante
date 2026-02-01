d323c52815962f12b365d8e022719bfdf249e0be
sultanaa21 <marosoultan1@gmail.com>
2026-02-01 11:31:43 +0100

    Mejora de SEO sitemap.xml

---
+++ app/sitemap.ts
@@ -0,0 +1,38 @@
+import { MetadataRoute } from 'next';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+    const baseUrl = 'https://ayni-restaurante.vercel.app';
+
+    return [
+        {
+            url: `${baseUrl}/`,
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 1,
+        },
+        {
+            url: `${baseUrl}/carta`,
+            lastModified: new Date(),
+            changeFrequency: 'weekly',
+            priority: 0.8,
+        },
+        {
+            url: `${baseUrl}/contacto`,
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 0.6,
+        },
+        {
+            url: `${baseUrl}/privacidad`,
+            lastModified: new Date(),
+            changeFrequency: 'yearly',
+            priority: 0.3,
+        },
+        {
+            url: `${baseUrl}/cookies`,
+            lastModified: new Date(),
+            changeFrequency: 'yearly',
+            priority: 0.3,
+        },
+    ];
+}
